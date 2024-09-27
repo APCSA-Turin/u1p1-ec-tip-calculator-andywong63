224671f83f9527c4230b71e60cbe2b1986c679ca
andywong63 <andyw63@nycstudents.net>
2024-09-27 01:34:51 +0000

    Incorporate suggestion

--- src/main/java/com/example/project/ExtraCredit.java
+++ src/main/java/com/example/project/ExtraCredit.java
@@ -3,32 +3,35 @@ import java.util.Scanner;
 
 
 public class ExtraCredit {
-    public static double roundToPlaces(double number, int places) {
-        double multiplyFactor = Math.pow(10, places);
-        return (int) (number * multiplyFactor + 0.5) / multiplyFactor;
+    public static String roundMoney(double number) {
+        return "$" + String.format("%.2f", number); // https://stackoverflow.com/a/27832159
     }
-    public static double roundDownToPlaces(double number, int places) {
-        double multiplyFactor = Math.pow(10, places);
-        return (int) (number * multiplyFactor) / multiplyFactor;
+    public static String roundMoneyDown(double number) {
+        number = (int) (number * 100) / 100.0;
+        return "$" + String.format("%.2f", number); // https://stackoverflow.com/a/27832159
     }
     public static String calculateTip(int people, int percent, double cost, String items) {
         StringBuilder result = new StringBuilder();
 
-                //COPY AND PASTE YOUR CODE HERE 
+        //COPY AND PASTE YOUR CODE HERE 
         double tip = (int) (cost * percent + 0.5) / 100.0;
         double totalCost = cost + tip;
-        double perPersonCost = roundToPlaces(cost / people, 2);
-        double perPersonTip = roundToPlaces(tip / people, 2);
-        double perPersonTotal = roundDownToPlaces(totalCost / people, 2);
+
+        String costRounded = roundMoney(cost);
+        String tipRounded = roundMoney(tip);
+        String perPersonCost = roundMoney(cost / people);
+        String perPersonTip = roundMoney(tip / people);
+        String perPersonTotal = roundMoneyDown(totalCost / people);
+        String totalCostRounded = roundMoney(totalCost);
                        
         result.append("-------------------------------\n");
-        result.append("Total bill before tip: " + "$" + cost + "\n"); //concatenate to this string. DO NOT ERASE AND REWRITE
+        result.append("Total bill before tip: " + costRounded + "\n"); //concatenate to this string. DO NOT ERASE AND REWRITE
         result.append("Total percentage: " + percent + "%\n");
-        result.append("Total tip: " + "$" + tip + "\n");
-        result.append("Total Bill with tip: " + "$" + totalCost + "\n");
-        result.append("Per person cost before tip: " + "$" + perPersonCost + "\n");
-        result.append("Tip per person: " + "$" + perPersonTip + "\n");
-        result.append("Total cost per person: " + "$" + perPersonTotal + "\n");
+        result.append("Total tip: " + tipRounded + "\n");
+        result.append("Total Bill with tip: " + totalCostRounded + "\n");
+        result.append("Per person cost before tip: " + perPersonCost + "\n");
+        result.append("Tip per person: " + perPersonTip + "\n");
+        result.append("Total cost per person: " + perPersonTotal + "\n");
         result.append("-------------------------------\n");
 
         //the two lines  should go below result.append("-------------------------------\n"); 
@@ -40,11 +43,14 @@ public class ExtraCredit {
     }
                                    
     public static void main(String[] args) {
-        int people = 12;
-        int percent = 15;
-        double cost = 566.97;
+        int people = 10;
+        int percent = 10;
+        double cost = 100;
         String items = "";
 
+        System.out.println(roundMoney(1.495));
+
+        //Your scanner object and while loop should go here
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
         while (!userInput.contains("-1")) {
@@ -55,8 +61,6 @@ public class ExtraCredit {
             }
         }
         scanner.close();
-
-        //Your scanner object and while loop should go here
                              
         System.out.println(calculateTip(people,percent,cost,items));
     }
